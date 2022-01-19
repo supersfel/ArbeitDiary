0c253b4544837d6617c46832144ef17bef90ee4b
supersfel <jmg081005@naver.com>
2022-01-19 18:53:49 +0900

    0119
    상단바, 유저목록 고정

--- arbeit-diary/src/components/Header.tsx
+++ arbeit-diary/src/components/Header.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../css/components/Header.css";
+import $ from "jquery";
 
 function Header() {
+  const [ScrollY, setScrollY] = useState(0); // window 의 pageYOffset값을 저장
+  const [ScrollActive, setScrollActive] = useState(false);
+  function handleScroll() {
+    if (ScrollY > 10) {
+      setScrollY(window.pageYOffset);
+      setScrollActive(true);
+    } else {
+      setScrollY(window.pageYOffset);
+      setScrollActive(false);
+    }
+  }
+  useEffect(() => {
+    function scrollListener() {
+      window.addEventListener("scroll", handleScroll);
+    } //  window 에서 스크롤을 감시 시작
+    scrollListener(); // window 에서 스크롤을 감시
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }; //  window 에서 스크롤을 감시를 종료
+  });
+
   return (
     <>
-      <header className="Header">
+      <header className={"Header" + (ScrollActive ? " onScrolled" : "")}>
         <Link to="/" className="left">
           로고
         </Link>
