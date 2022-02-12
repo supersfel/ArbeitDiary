aace557b416b06fb75063b6e442dd978e28e472a
supersfel <81850778+supersfel@users.noreply.github.com>
2022-02-13 02:39:05 +0900

    ArbeitDiary 1.3.10
    1.3.10 IndexPage 추가작업

--- FE/arbeit-diary/src/components/Introduce.tsx
+++ FE/arbeit-diary/src/components/Introduce.tsx
@@ -3,8 +3,36 @@ import "../css/containers/Introduce.css";
 function Introduce() {
   return (
     <div className="Introduce">
-      <div className="comment">매장만을 위한 ,</div>
-      <div className="shadowBox intro"></div>
+      <div className="comment">함께 만들어 나가는 ,</div>
+      <div className="shadowBox intro">
+        <div className="Box first">
+          아르바이트생도
+          <div className="staff">
+            <img
+              className="img"
+              src="https://img.icons8.com/external-icongeek26-flat-icongeek26/100/000000/external-waiter-food-and-delivery-icongeek26-flat-icongeek26-1.png"
+            />
+            <img
+              className="img"
+              src="https://img.icons8.com/external-icongeek26-flat-icongeek26/100/000000/external-waiter-food-and-delivery-icongeek26-flat-icongeek26.png"
+            />
+          </div>
+        </div>
+        <div className="Box first">
+          관리자도
+          <img
+            className="img"
+            src="https://img.icons8.com/external-filled-outline-icons-maxicons/100/000000/external-boss-office-space-filled-outline-filled-outline-icons-maxicons.png"
+          />
+        </div>
+      </div>
+      <div className="comment">정말 간편한 ,</div>
+      <div className="shadowBox intro">
+        <div className="Box">
+          손쉽게 클릭 한번으로 !
+          <div className="makeprojectimg img"></div>
+        </div>
+      </div>
     </div>
   );
 }
