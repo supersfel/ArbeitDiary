979e5626159d224f7022d00facc03013e6778aaa
supersfel <81850778+supersfel@users.noreply.github.com>
2022-02-19 22:54:58 +0900

    ArbeitDiary 2.0.5_0219

---
+++ BE/ZB/fastlms/src/main/resources/static/precache-manifest.3225dd425187e862dfdacdebbabcc42e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75fe45ac5520e2812de453ae6b25efb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a21cd0317f021d91124",
+    "url": "/static/css/main.ca5a912a.chunk.css"
+  },
+  {
+    "revision": "35c738735adffa67484f",
+    "url": "/static/js/2.60abe10e.chunk.js"
+  },
+  {
+    "revision": "a1d56634ec02de80840d0707b1576bc9",
+    "url": "/static/js/2.60abe10e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b34bf2ce3771acca581",
+    "url": "/static/js/3.182b6ee0.chunk.js"
+  },
+  {
+    "revision": "1a21cd0317f021d91124",
+    "url": "/static/js/main.fca3f53c.chunk.js"
+  },
+  {
+    "revision": "0931762a772936d1a62e",
+    "url": "/static/js/runtime-main.4ff0ee9e.js"
+  },
+  {
+    "revision": "415e040912eb9897107ecdc85cecf581",
+    "url": "/static/media/iconlogo.415e0409.png"
+  },
+  {
+    "revision": "820cc69d8af0ca127ddec9c648df35d3",
+    "url": "/static/media/logo1.820cc69d.png"
+  },
+  {
+    "revision": "eed273579ba10f404848f1f4c52eef55",
+    "url": "/static/media/logo2.eed27357.png"
+  },
+  {
+    "revision": "356725173ff632525cf32f15b0dbcea5",
+    "url": "/static/media/makeproject-img.35672517.PNG"
+  }
+]);
